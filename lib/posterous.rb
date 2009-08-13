42541578da66201bbbf91a24dcb1829bef08356f
Rich Manalang <rich.manalang@gmail.com>
2009-08-13 10:53:42 -0700

    new app

---
+++ lib/posterous.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'httparty'
+
+class Posterous
+  include HTTParty
+  base_uri 'posterous.com'
+
+  def initialize(mail, password)
+    self.class.basic_auth mail,password
+  end
+
+  def sites
+    self.class.get('/api/getsites')
+  end
+
+  def newpost(title, body, autopost = 0)
+    self.class.get('/api/newpost', {:query => {:title => title, :body => body, :autopost => autopost}})
+  end
+end
